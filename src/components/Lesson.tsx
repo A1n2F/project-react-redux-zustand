2e887dfc59a944cdfa262e36fe93229e7ed3818a
A1n2F <alissonfferreira22@gmail.com>
2025-03-21 00:12:21 -0300

    Aplicação com Redux - Selecionando aula atual

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -3,11 +3,12 @@ import { Video } from "lucide-react";
 interface LessonProps {
     title: string
     duration: string
+    onPlay: () => void
 }
 
-export function Lesson({ title, duration }: LessonProps) {
+export function Lesson({ title, duration, onPlay }: LessonProps) {
     return (
-        <button className="flex items-center gap-3 text-md text-zinc-400">
+        <button onClick={onPlay} className="flex items-center gap-3 text-md text-zinc-400">
             <Video className="w-5 h-5 text-zinc-500" />
             <span>{title}</span>
             <span className="ml-auto font-mono text-sm text-zinc-500">{duration}</span>
